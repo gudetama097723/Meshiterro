c8d289969dbd31f67d7d87137973b72c111e9462
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-06-04 11:01:35 +0000

    なんかコミットしてなかったやつ

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  normalizes :email_address, with: ->(e) { e.strip.downcase }
+  normalizes :email, with: ->(e) { e.strip.downcase }
 
   has_one_attached :profile_image
 
--- db/schema.rb
+++ db/schema.rb
@@ -72,12 +72,12 @@ ActiveRecord::Schema[8.0].define(version: 2026_04_14_035746) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
